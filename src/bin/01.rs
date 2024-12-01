a548196ec19974a53238e29096c56cf79c8b8011
lpnh <paniguel.lpnh@gmail.com>
2024-12-01 13:10:43 -0300

    solve Day 1 Part One

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,7 +4,24 @@ const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_01.txt");
 
 #[cfg(feature = "part_1")]
 fn solve_part_1(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    let mut left_ids: Vec<i32> = vec![];
+    let mut right_ids: Vec<i32> = vec![];
+
+    input.lines().for_each(|line| {
+        let mut pairs = line.split_whitespace();
+        left_ids.push(pairs.next().unwrap().parse::<i32>().unwrap());
+        right_ids.push(pairs.next().unwrap().parse::<i32>().unwrap());
+    });
+
+    left_ids.sort();
+    right_ids.sort();
+
+    let solution = left_ids
+        .iter()
+        .zip(right_ids.iter())
+        .map(|(left, right)| i32::abs(left - right))
+        .sum::<i32>()
+        .to_string();
 
     Ok(solution)
 }
@@ -40,11 +57,14 @@ fn main() -> Result<(), Error> {
 #[test]
 fn sample_part_1() {
     const SAMPLE_INPUT_1: &str = "\
-sample part 1 input
-goes here
-like this
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 ";
-    const SAMPLE_ANSWER_1: &str = "sample part 1 answer";
+    const SAMPLE_ANSWER_1: &str = "11";
 
     assert_eq!(solve_part_1(SAMPLE_INPUT_1).unwrap(), SAMPLE_ANSWER_1);
 }
