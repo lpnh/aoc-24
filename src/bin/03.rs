8935836f730c78838f41563707075473113a8f61
lpnh <paniguel.lpnh@gmail.com>
2024-12-06 10:30:09 -0300

    solve Day 3 Part One

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,12 +1,23 @@
 use anyhow::{Error, Result};
+use regex::Regex;
 
 const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_03.txt");
 
 #[cfg(feature = "part_1")]
 fn solve_part_1(input: &str) -> Result<String, Error> {
-    let solution = input.lines().next().unwrap().replace("input", "answer");
+    let memory = input;
+    let re = Regex::new(r"mul\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)").unwrap();
 
-    Ok(solution)
+    let mut sum = 0;
+
+    for cap in re.captures_iter(memory) {
+        let n1 = &cap[1].parse::<u32>().unwrap();
+        let n2 = &cap[2].parse::<u32>().unwrap();
+
+        sum += n1 * n2;
+    }
+
+    Ok(sum.to_string())
 }
 
 #[cfg(feature = "part_2")]
@@ -40,11 +51,9 @@ fn main() -> Result<(), Error> {
 #[test]
 fn sample_part_1() {
     const SAMPLE_INPUT_1: &str = "\
-sample part 1 input
-goes here
-like this
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ";
-    const SAMPLE_ANSWER_1: &str = "sample part 1 answer";
+    const SAMPLE_ANSWER_1: &str = "161";
 
     assert_eq!(solve_part_1(SAMPLE_INPUT_1).unwrap(), SAMPLE_ANSWER_1);
 }
